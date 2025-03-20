c71e6a6c4e51959880e97c8579af603171375add
Oscar Lara <oscar.lara@unosquare.com>
2025-03-20 11:23:07 -0600

    chore: set eslint up

---
+++ frontend/prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true, // Use semicolons
+    singleQuote: true, // Use single quotes
+    trailingComma: 'all', // Add trailing commas
+    printWidth: 80, // Line width
+    tabWidth: 2, // Indent with 2 spaces
+    useTabs: false, // Use spaces instead of tabs
+    endOfLine: 'auto', // Handle line endings automatically
+  };
